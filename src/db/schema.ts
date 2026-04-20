f635108f0410ab5b356fbb92f51d798a2cd34511
radsylph <andresalvarezsilva6@gmail.com>
2026-04-20 16:37:11 -0400

    feat:added the initial task schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,17 @@
-import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+import { boolean, pgTable, uuid, text, varchar, timestamp, pgEnum } from 'drizzle-orm/pg-core'
 
-export const todos = pgTable('todos', {
-  id: serial().primaryKey(),
-  title: text().notNull(),
-  createdAt: timestamp('created_at').defaultNow(),
+export const statusEnum = pgEnum('status', ['active', 'inactive', 'completed'])
+export const priorityEnum = pgEnum('priority', ['low', 'medium', 'high'])
+
+export const task = pgTable('tasks', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  title: varchar('title', { length: 255 }).notNull(), // the type varchar can be limited in the lenght of the string
+  description: text('description'), // the type text is unbounded
+  status: statusEnum('status').default('active').notNull(),
+  priority: priorityEnum('priority').default('low').notNull(),
+  isDeleted: boolean('is_deleted').default(false).notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
+  deletedAt: timestamp('deleted_at'),
 })
+
