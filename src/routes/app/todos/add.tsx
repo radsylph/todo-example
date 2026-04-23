62e67b153e73161f34c43f15bb33dbf12152c657
radsylph <andresalvarezsilva6@gmail.com>
2026-04-22 20:04:46 -0400

    chore:changed the name of the folder

--- src/routes/app/todos/add.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/app/todos/add')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/app/todos/add"!</div>
-}
