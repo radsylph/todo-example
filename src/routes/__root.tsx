6a9dfbf285390df529a702a4db8b75bf4dabc5c1
radsylph <andresalvarezsilva6@gmail.com>
2026-04-29 10:37:31 -0400

    feat:added Toaster component to the app root layout.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,6 +6,7 @@ import { NotFoundComponent } from '../modules/common/components/notFoundComponen
 import { getLocale } from '#/paraglide/runtime'
 
 import appCss from '../styles.css?url'
+import { Toaster } from '#components/ui/sonner'
 
 // const THEME_INIT_SCRIPT = `(function(){try{var stored=window.localStorage.getItem('theme');var mode=(stored==='light'||stored==='dark'||stored==='auto')?stored:'auto';var prefersDark=window.matchMedia('(prefers-color-scheme: dark)').matches;var resolved=mode==='auto'?(prefersDark?'dark':'light'):mode;var root=document.documentElement;root.classList.remove('light','dark');root.classList.add(resolved);if(mode==='auto'){root.removeAttribute('data-theme')}else{root.setAttribute('data-theme',mode)}root.style.colorScheme=resolved;}catch(e){}})();`
 // hacer otra implementación con zustand para el manejo de los temas.
@@ -63,6 +64,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
           ]}
         />
         <Scripts />
+        <Toaster position='top-center' richColors expand/>
       </body>
     </html>
   )
