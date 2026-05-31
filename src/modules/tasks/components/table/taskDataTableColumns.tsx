649edad6d53e25eb92b828f2bffffdf09734d390
radsylph <andresalvarezsilva6@gmail.com>
2026-05-31 11:12:56 -0400

    refactor: remove header and cell definitions from task columns and update priority filter variant to multiSelect

--- src/modules/tasks/components/table/taskDataTableColumns.tsx
+++ src/modules/tasks/components/table/taskDataTableColumns.tsx
@@ -1,21 +1,11 @@
-import DataTableBadgeCell from '#components/layout/dataTable/dataTableBadgeCell';
-import DataTableTextCell from '#components/layout/dataTable/dataTableTextCell';
 import { m } from '#/paraglide/messages';
-import { DataTableColumnHeader } from '#components/layout/dataTable/dataTableColumnHeader';
 import { type ColumnDef } from '@tanstack/react-table';
 import { type Task, TaskStatus, TaskPriority } from '../../types';
+
 export const taskColumns: ColumnDef<Task>[] = [
   {
     id:"title",
     accessorKey: "title",
-    header: ({ column }) => (
-      <DataTableColumnHeader
-        column={column}
-        title={m.task_form_title_label()}
-        
-      />
-    ),
-    cell: ({ row }) => <DataTableTextCell text={row.original.title} />,
     enableSorting: true,
     enableColumnFilter: false,
     meta: {
@@ -26,21 +16,6 @@ export const taskColumns: ColumnDef<Task>[] = [
   {
     id:"status",
     accessorKey: "status",
-    header: ({ column }) => (
-      <DataTableColumnHeader
-        column={column}
-        title={m.task_form_status_label()}
-      />
-    ),
-    cell: ({ row }) => {
-      const status = row.original.status  ;
-      return (
-        <DataTableBadgeCell
-          text={status}
-          variant={status === TaskStatus.COMPLETED ? "default" : "outline"}
-        />
-      );
-    },
     enableSorting: true,
     enableColumnFilter: true,
     meta: {
@@ -56,32 +31,11 @@ export const taskColumns: ColumnDef<Task>[] = [
   {
     id:"priority",
     accessorKey: "priority",
-    header: ({ column }) => (
-      <DataTableColumnHeader
-        column={column}
-        title={m.task_form_priority_label()}
-      />
-    ),
-    cell: ({ row }) => {
-      const priority = row.original.priority as TaskPriority;
-      return (
-        <DataTableBadgeCell
-          text={priority}
-          variant={
-            priority === TaskPriority.HIGH
-              ? "destructive"
-              : priority === TaskPriority.MEDIUM
-                ? "secondary"
-                : "outline"
-          }
-        />
-      );
-    },
     enableSorting: true,
     enableColumnFilter: true,
     meta: {
       label: m.task_form_priority_label(),
-      variant: "select",
+      variant: "multiSelect",
       options: [
         { label: m.priority_high(), value: TaskPriority.HIGH },
         { label: m.priority_medium(), value: TaskPriority.MEDIUM },
